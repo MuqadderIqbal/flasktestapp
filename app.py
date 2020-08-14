90c3b41227677c3fbeee6ab9774b1542298e1262
Muqadder Iqbal <muqadderiqbal@ymail.com>
2020-08-14 02:27:47 -0700

    cleanup

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+#import pyttsx3 as ptx
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #ptx.speak("Welcome")
     return "<h1> Hello World 123! </h1>"
 
 if __name__ == "__main__":
